19a87225140582ed16cd7d6e5999c02f781098a1
Ulad Shumeika <wladshumeiko228@gmail.com>
2023-04-03 10:23:51 +0200

    [_misc.h] feat: add USH_NVIC_priorityGroup enumeration

--- Inc/ush_stm32f4xx_misc.h
+++ Inc/ush_stm32f4xx_misc.h
@@ -56,6 +56,27 @@ typedef enum
 } USH_PWR_flags;
 
 /**
+ * @brief Preemption priority group enumeration
+ */
+typedef enum
+{
+	NVIC_PRIORITYGROUP_0	= 0x00000007U,		/* 0 bits for pre-emption priority
+	 	 	 	 	 	 	 	 	 	 	 	   4 bits for subpriority */
+
+	NVIC_PRIORITYGROUP_1	= 0x00000006U,		/* 1 bits for pre-emption priority
+												   3 bits for subpriority */
+
+	NVIC_PRIORITYGROUP_2	= 0x00000005U,		/* 2 bits for pre-emption priority
+												   2 bits for subpriority */
+
+	NVIC_PRIORITYGROUP_3	= 0x00000004U,		/* 3 bits for pre-emption priority
+												   1 bits for subpriority */
+
+	NVIC_PRIORITYGROUP_4	= 0x00000003U,		/* 4 bits for pre-emption priority
+												   0 bits for subpriority */
+} USH_NVIC_priorityGroup;
+
+/**
  * @brief Flash latency enumeration
  */
 typedef enum
