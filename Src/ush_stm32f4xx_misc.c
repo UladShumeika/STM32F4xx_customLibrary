a2c1f88058bc3687a42e0bd2eb819ebc492031d8
Ulad Shumeika <wladshumeiko228@gmail.com>
2023-04-03 10:42:22 +0200

    [_misc.h/_misc.c] docs: change driver's version and add major changes section

--- Src/ush_stm32f4xx_misc.c
+++ Src/ush_stm32f4xx_misc.c
@@ -2,7 +2,7 @@
   ******************************************************************************
   * @file    ush_stm32f4xx_misc.c
   * @author  Ulad Shumeika
-  * @version v1.0
+  * @version v1.1
   * @date    27-February-2023
   * @brief	 This file contains the implementation of functions for the miscellaneous
   * 		 firmware library functions.
