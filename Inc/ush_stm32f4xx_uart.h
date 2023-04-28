ba27b55bf4f85ec328f6e4679accb9a998186daa
Ulad Shumeika <wladshumeiko228@gmail.com>
2023-04-28 14:18:30 +0200

    [_uart.h] docs: add changes' description

--- Inc/ush_stm32f4xx_uart.h
+++ Inc/ush_stm32f4xx_uart.h
@@ -2,12 +2,17 @@
   ******************************************************************************
   * @file    ush_stm32f4xx_usart.h
   * @author  Ulad Shumeika
-  * @version v1.0
+  * @version v1.1
   * @date    21-February-2023
   * @brief   Header file of U(S)ART module.
   *
   * NOTE: This file is not a full-fledged U(S)ART driver, but contains only some of
   * 	  the functions that are needed for the current project.
+  *
+  *
+  * @Major changes v1.1
+  *		- USART_getDmaStream function changed and moved to "static" section;
+  *
   ******************************************************************************
   */
 
