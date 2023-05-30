fe64ecaff21c3edf68e65eb4ae25ea8c681b1632
Ulad Shumeika <wladshumeiko228@gmail.com>
2023-05-30 15:51:43 +0200

    [_i2c.c] docs: add file description

--- Src/ush_stm32f4xx_i2c.c
+++ Src/ush_stm32f4xx_i2c.c
@@ -1,3 +1,18 @@
+/**
+  ******************************************************************************
+  * @file    ush_stm32f4xx_i2c.c
+  * @author  Ulad Shumeika
+  * @version v1.0
+  * @date    27 May 2023
+  * @brief	 This file contains the implementation of functions for working with
+  * 		 I2C bus.
+  *
+  * NOTE: This driver is not a full-fledged I2C driver, but contains only some of
+  * 	  the functions were necessary at the time of development of this driver.
+  * 	  However, if necessary, the function of this driver will be expanded.
+  ******************************************************************************
+  */
+
 //---------------------------------------------------------------------------
 // Includes
 //---------------------------------------------------------------------------
